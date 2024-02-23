c9cd279118d7c1a3390a73360f1c9fa99f48a5f6
maik <michaelqp56@educastur.es>
2024-02-23 19:20:54 +0100

    Añadidos comentarios de documentación en claseExam

--- src/miExamen/claseExamen.java
+++ src/miExamen/claseExamen.java
@@ -1,20 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package miExamen;
 
 /**
- *
- * @author alu03v
+ * @author Michael Quintero Petroche
+ * Entornos de desarrollo UT3
  */
 public class claseExamen {
 
     /**
      * @param args the command line arguments
+     * @version 1.0.0
+     * @since 23/02/2024
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /* Esta clase sirve para realizar el examen de la UT3 del modulo 
+        * Entornos de Desarrollo
+        * Curso de Git y github de píldoras informáticas.
+        * https://www.youtube.com/playlist?list=PLU8oAlHdN5BlyaPFiNQcV0xDqy0eR35aU
+        */
+        System.out.println("Estoy haciendo el examen de la UT3");
     }
     
 }
